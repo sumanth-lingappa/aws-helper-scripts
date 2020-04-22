e3a782a5b770dd266f91fe76d177a6d9c734e133
Sumanth Lingappa <sumanth.lingappa@citrix.com>
2020-04-22 16:01:19 +0530

    release unassociated eips

---
+++ release_unassociated_eips.py
@@ -0,0 +1,18 @@
+import boto3
+
+client = boto3.client('ec2')
+
+
+def get_unassociated_eips():
+    return [x for x in client.describe_addresses()['Addresses'] if 'AssociationId' not in x]
+
+
+def release_eips(eip_list):
+    for eip in eip_list:
+        client.release_address(AllocationId=eip['AllocationId'])
+        print("Released EIP: {}".format(eip['PublicIp']))
+
+
+if __name__ == "__main__":
+    unassociated_eips = get_unassociated_eips()
+    release_eips(unassociated_eips)
